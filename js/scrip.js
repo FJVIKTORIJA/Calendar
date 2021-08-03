0a73cb6034fb3a2e33b7c8c83076a396327da565
FJVIKTORIJA <83015268+FJVIKTORIJA@users.noreply.github.com>
2021-08-03 20:38:44 +0300

    Updating IDs

--- js/scrip.js
+++ js/scrip.js
@@ -1,29 +1,50 @@
 var today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 
-
-
-
-var eventFormEl = $('#Todo');
-var eventListEl = $('#userinput');
+var eventFormEl = $("#Todo");
+var eventListEl = $("#userinput");
 
 function handleFormSubmit(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    var myevent = $('input[name="plan-input"]').val();
+  var myevent = $('input[name="plan-input"]').val();
 
-    if (!myevent) {
-        console.log('No event added!');
-        return;
-      }
-      var myeventlistEl = $(
+  if (!myevent) {
+    console.log("No event added!");
+    return;
+  }
+  var myeventlistEl = $();
+  myeventlistEl.text(myevent);
 
-      );
-      myeventlistEl.text(myevent);
-
-      myeventlistEl.append(
-        
-      );
-      eventlistEl.append(myeventlistEl);
-      
+  myeventlistEl.append();
+  eventlistEl.append(myeventlistEl);
 }
+
+//Galbut
+const form = document.getElementById("form");
+const log = document.getElementById("log");
+form.addEventListener("submit", logSubmit);
+
+/*
+Any time an html form submits, the default behavior is that it makes a post request and reloads the page. So to prevent the page from reloading, we need that line you have that says event.preventDefault(); because it stops the page from reloading.
+white_check_mark
+eyes
+raised_hands
+
+
+
+
+
+2:56
+But if the function is not connected to the form, then the code inside the function will not run when the form submits
+2:56
+And the default reload will not be prevented
+New
+2:57
+So make sure to add some code to your javascript file that selects all the forms from the DOM and then add your function as an event listener to the forms
+
+//use
+https://api.jquery.com/submit/
+
+You can also use jQuery’s .on method, which can be used to assign any kind of event listener. You just have to specify that you want a submit event listener by passing it a string as the first argument https://api.jquery.com/on/
+*/
