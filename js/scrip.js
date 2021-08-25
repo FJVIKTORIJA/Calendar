66c625938f576d4d6c631780f8c1afaafee64047
FJVIKTORIJA <83015268+FJVIKTORIJA@users.noreply.github.com>
2021-08-25 22:11:09 +0100

    Added the local storage and past, present , future colours based on current time

--- js/scrip.js
+++ js/scrip.js
@@ -1,50 +1,46 @@
 var today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 
-var eventFormEl = $("#Todo");
-var eventListEl = $("#userinput");
-
-function handleFormSubmit(event) {
+$(".saveBtn").on("click", function (event) {
   event.preventDefault();
+  var value = $(this).parent().find("#userinput").val();
+  var time = $(this).parent().find("label").attr("id");
+  localStorage.setItem(time, value);
+});
 
-  var myevent = $('input[name="plan-input"]').val();
+updateHour();
+function updateHour() {
+  var timeNow = moment().hour();
 
-  if (!myevent) {
-    console.log("No event added!");
-    return;
-  }
-  var myeventlistEl = $();
-  myeventlistEl.text(myevent);
-
-  myeventlistEl.append();
-  eventlistEl.append(myeventlistEl);
+  $(".row").each(function () {
+    var currentHour = parseInt($(this).find("label").attr("id").split("-")[1]);
+    console.log(currentHour);
+    if (currentHour < timeNow) {
+      $(this).addClass("past");
+    } else if (currentHour === timeNow) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
+  });
 }
 
-//Galbut
-const form = document.getElementById("form");
-const log = document.getElementById("log");
-form.addEventListener("submit", logSubmit);
+document.getElementById("userinput").value = getSavedValue("userinput"); // set the value to this input
 
-/*
-Any time an html form submits, the default behavior is that it makes a post request and reloads the page. So to prevent the page from reloading, we need that line you have that says event.preventDefault(); because it stops the page from reloading.
-white_check_mark
-eyes
-raised_hands
+/* Here you can add more inputs to set value. if it's saved */
 
+//Save the value function - save it to localStorage as (ID, VALUE)
+function saveValue(e) {
+  var id = e.id; // get the sender's id to save it .
+  var val = e.value; // get the value.
+  localStorage.setItem(id, val); // Every time user writing something, the localStorage's value will override .
+}
 
-
-
-
-2:56
-But if the function is not connected to the form, then the code inside the function will not run when the form submits
-2:56
-And the default reload will not be prevented
-New
-2:57
-So make sure to add some code to your javascript file that selects all the forms from the DOM and then add your function as an event listener to the forms
-
-//use
-https://api.jquery.com/submit/
-
-You can also use jQuery’s .on method, which can be used to assign any kind of event listener. You just have to specify that you want a submit event listener by passing it a string as the first argument https://api.jquery.com/on/
-*/
+//get the saved value function - return the value of "v" from localStorage.
+function getSavedValue(v) {
+  if (!localStorage.getItem(v)) {
+    return ""; // You can change this to your defualt value.
+  }
+  return localStorage.getItem(v);
+}
+// function that checks localStorage and updates each input for the time
